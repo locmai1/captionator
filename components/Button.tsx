bcc485ec4d7e0866a256a27944897f67c73ad593
Loc Mai <locmai0315@gmail.com>
2023-07-01 01:13:44 -0400

    style: resize primary button

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({
   return (
     <button
       type="button"
-      className="my-2 py-3 px-6 inline-flex justify-center items-center gap-2 rounded-md border-2 border-black/80 font-semibold text-black hover:text-white hover:bg-black/80 hover:border-gray-800 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 transition-all text-md"
+      className="my-2 py-2 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-black font-semibold text-black hover:text-white hover:bg-black hover:border-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 transition-all text-md"
       onClick={onClick}
     >
       {icon}
