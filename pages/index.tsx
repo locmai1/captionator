57e878e36832fff8bc21707846733a24b07c1d33
Loc Mai <locmai0315@gmail.com>
2023-07-01 01:17:04 -0400

    chore: implement rate limiter on frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import { NextPage } from "next";
 import { useState } from "react";
+import { useSession, signIn } from "next-auth/react";
 import { UploadDropzone } from "react-uploader";
 import { Uploader } from "uploader";
 import { AnimatePresence, motion } from "framer-motion";
+import useSWR from "swr";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
@@ -12,7 +14,6 @@ import ResizablePanel from "../components/ResizablePanel";
 import Button from "../components/Button";
 import GoogleIcon from "../components/GoogleIcon";
 import copyToClipboard from "../utils/copyToClipboard";
-import { useSession, signIn } from "next-auth/react";
 
 // Configuration for the uploader
 const uploader = Uploader({
@@ -33,16 +34,15 @@ const options = {
       shade900: "#ffffff", // Various (draggable crop buttons, etc.)
     },
   },
-  showRemoveButton: false,
-  showFinishButton: false,
 };
 
 const Home: NextPage = () => {
+  const fetcher = () => fetch("/api/remaining").then((res) => res.json());
   const [photo, setPhoto] = useState<string | null>(null);
   const [caption, setCaption] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const [reset, setReset] = useState<string | null>(null);
+  const { data, mutate, isLoading } = useSWR("/api/remaining", fetcher);
   const { status } = useSession();
 
   const UploadDropZone = () => (
@@ -72,12 +72,10 @@ const Home: NextPage = () => {
 
     const response = await res.json();
     if (res.status !== 200) {
-      if (response.reset) {
-        setReset(response.reset);
-      }
       setError(response.error);
     } else {
       setCaption(response.caption);
+      mutate();
     }
     setLoading(false);
     console.log(response);
@@ -92,127 +90,136 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 sm:mb-10 mt-10 mb-5">
-        <h1 className="mx-auto max-w-4xl font-display text-3xl font-bold tracking-normal text-slate-900 sm:text-7xl mb-7 sm:mb-20">
-          Translating all photos{" "}
-          <span className="relative whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-[#E1306C] to-[#833AB4]">
-            using AI
-          </span>{" "}
-          into captions.
-        </h1>
-        <ResizablePanel>
-          <AnimatePresence mode="wait">
-            <motion.div className="flex justify-between items-center w-full flex-col">
-              {status === "loading" ? (
-                <button
-                  disabled
-                  className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 hover:bg-black/80 w-40 my-2"
-                >
-                  <span className="pt-4">
-                    <LoadingDots color="white" />
-                  </span>
-                </button>
-              ) : status === "authenticated" && !photo && !reset ? (
-                <UploadDropZone />
-              ) : (
-                !photo && (
-                  <Button
-                    text="Sign in with Google"
-                    icon={<GoogleIcon />}
-                    onClick={() => signIn("google")}
-                  />
-                )
-              )}
-              {caption && photo && (
-                <div className="flex flex-col h-full">
-                  <div>
-                    <p className="mx-auto w-72 sm:w-96 text-xs sm:text-base text-slate-500 leading-7 sm:mb-1">
-                      Click on the{" "}
-                      <span className="relative font-bold">caption</span> below
-                      to copy it to clipboard
-                    </p>
-                    <button
-                      className="border border-dashed border-black rounded-lg w-72 sm:w-96 p-2 h-14 sm:h-20 mb-4"
-                      onClick={() => copyToClipboard(caption, setCaption)}
-                    >
-                      <h2 className="font-medium text-sm sm:text-lg">
-                        {caption}
-                      </h2>
-                    </button>
-                    <Image
-                      alt="photo"
-                      src={photo}
-                      className="rounded-2xl max-w-[300px] sm:max-w-[500px] shadow-lg"
-                      width={500}
-                      height={500}
-                    />
-                  </div>
-                </div>
-              )}
-              {loading && (
-                <button
-                  disabled
-                  className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 hover:bg-black/80 w-40"
-                >
-                  <span className="pt-4">
-                    <LoadingDots color="white" />
-                  </span>
-                </button>
-              )}
-              <div className="flex space-x-2 justify-center">
-                {photo && !loading && !error && (
-                  <button
-                    onClick={() => {
-                      setPhoto(null);
-                      setCaption(null);
-                      setError(null);
-                    }}
-                    className="bg-black rounded-full text-white font-medium px-4 py-2 hover:bg-black/80 transition mt-6"
-                  >
-                    Upload New Photo
-                  </button>
-                )}
-              </div>
-              {error && !reset && (
-                <div>
+        {!isLoading ? (
+          <ResizablePanel>
+            <AnimatePresence mode="wait">
+              <motion.div className="flex justify-between items-center w-full flex-col">
+                <h1 className="mx-auto max-w-4xl font-display text-3xl font-bold tracking-normal text-slate-900 sm:text-7xl mb-7 sm:mb-20">
+                  Translating all photos{" "}
+                  <span className="relative whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-[#E1306C] to-[#833AB4]">
+                    using AI
+                  </span>{" "}
+                  into captions.
+                </h1>
+                {data.remaining == 0 && !photo && (
                   <div
-                    className="mx-auto w-64 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                    className="mx-auto w-64 bg-red-100 border border-red-400 text-red-700 px-6 py-3 rounded relative"
                     role="alert"
                   >
-                    <span className="block sm:inline">{error}</span>
+                    <span className="block sm:inline">
+                      Your generations will reset on{" "}
+                      <span className="font-semibold">{data.reset}</span>
+                    </span>
                   </div>
-                  <p className="mx-auto sm:w-96 text-slate-500 leading-7 mt-1">
-                    Click{" "}
+                )}
+                {status === "authenticated" && !photo && data.remaining != 0 ? (
+                  <>
+                    <p className="text-slate-500">
+                      You have{" "}
+                      <span className="font-semibold">
+                        {data.remaining} generations
+                      </span>{" "}
+                      left today.
+                    </p>
+                    <UploadDropZone />
+                  </>
+                ) : (
+                  data.remaining != 0 &&
+                  !photo && (
+                    <Button
+                      text="Sign in with Google"
+                      icon={<GoogleIcon />}
+                      onClick={() => signIn("google")}
+                    />
+                  )
+                )}
+                {caption && photo && (
+                  <div className="flex flex-col h-full">
+                    <div>
+                      <p className="mx-auto w-72 sm:w-96 text-xs sm:text-base text-slate-500 leading-7 sm:mb-1">
+                        Click on the{" "}
+                        <span className="relative font-bold">caption</span>{" "}
+                        below to copy it to clipboard
+                      </p>
+                      <button
+                        className="border border-dashed border-black rounded-lg w-72 sm:w-96 p-2 h-14 sm:h-20 mb-4"
+                        onClick={() => copyToClipboard(caption, setCaption)}
+                      >
+                        <h2 className="font-medium text-sm sm:text-lg">
+                          {caption}
+                        </h2>
+                      </button>
+                      <Image
+                        alt="photo"
+                        src={photo}
+                        className="rounded-2xl max-w-[300px] sm:max-w-[500px] shadow-lg"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                  </div>
+                )}
+                {loading && (
+                  <button
+                    disabled
+                    className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 hover:bg-black/80 w-40"
+                  >
+                    <span className="pt-4">
+                      <LoadingDots color="white" />
+                    </span>
+                  </button>
+                )}
+                <div className="flex space-x-2 justify-center">
+                  {photo && !loading && !error && (
                     <button
-                      className="relative font-bold underline"
                       onClick={() => {
                         setPhoto(null);
                         setCaption(null);
                         setError(null);
                       }}
+                      className="bg-black rounded-full text-white font-medium px-4 py-2 hover:bg-black/80 transition mt-6"
                     >
-                      here
-                    </button>{" "}
-                    to retry
-                  </p>
+                      Upload New Photo
+                    </button>
+                  )}
                 </div>
-              )}
-              {reset && (
-                <div>
-                  <div
-                    className="mx-auto w-64 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                    role="alert"
-                  >
-                    <span className="block sm:inline">{error}</span>
+                {error && (
+                  <div>
+                    <div
+                      className="mx-auto w-64 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                      role="alert"
+                    >
+                      <span className="block sm:inline">{error}</span>
+                    </div>
+                    <p className="mx-auto sm:w-96 text-slate-500 leading-7 mt-1">
+                      Click{" "}
+                      <button
+                        className="relative font-bold underline"
+                        onClick={() => {
+                          setPhoto(null);
+                          setCaption(null);
+                          setError(null);
+                        }}
+                      >
+                        here
+                      </button>{" "}
+                      to retry
+                    </p>
                   </div>
-                  <p className="mx-auto sm:w-96 text-slate-500 leading-7 mt-1">
-                    Requests will reset @{" "}
-                    <span className="font-bold">{reset}</span>.
-                  </p>
-                </div>
-              )}
-            </motion.div>
-          </AnimatePresence>
-        </ResizablePanel>
+                )}
+              </motion.div>
+            </AnimatePresence>
+          </ResizablePanel>
+        ) : (
+          <button
+            disabled
+            className="bg-black rounded-full text-white font-medium px-4 pt-2 pb-3 hover:bg-black/80 w-40"
+          >
+            <span className="pt-4">
+              <LoadingDots color="white" />
+            </span>
+          </button>
+        )}
       </main>
       <Footer />
     </div>
