e76954d491456c62b0030337c1c958727e59739f
Loc Mai <locmai0315@gmail.com>
2023-07-12 15:51:04 -0400

    change x to svg icon

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -14,13 +14,26 @@ export default function Modal({
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex jusitfy-center items-center z-50">
       <div className="m-auto flex flex-col">
-        <button
-          className="text-xl text-red-500 place-self-end"
-          onClick={() => onClose()}
-        >
-          X
+        <button className="place-self-end" onClick={() => onClose()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            width="24px"
+            height="24px"
+          >
+            <path
+              fill="#F44336"
+              d="M21.5 4.5H26.501V43.5H21.5z"
+              transform="rotate(45.001 24 24)"
+            />
+            <path
+              fill="#F44336"
+              d="M21.5 4.5H26.5V43.501H21.5z"
+              transform="rotate(135.008 24 24)"
+            />
+          </svg>
         </button>
-        <div className="bg-white p-4 rounded">{children}</div>
+        <div className="bg-white p-6 rounded-md">{children}</div>
       </div>
     </div>
   );
